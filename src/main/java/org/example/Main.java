01273e7b1a8c4fd1a7ac2e7b90f3a5d0ce4e000e
User <oabdumalikov912@gmail.com>
2023-03-09 18:57:13 +0500

    hammasini tort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,5 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(45654);
-        System.out.println("sfsdgjhfjkdghkkjf");
-    }
+
 }
