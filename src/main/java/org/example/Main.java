8b52adf94fb5c312c3222becb5f43da1c455388f
OneComputers <uxasanov798@gmail.com>
2023-03-09 19:17:33 +0500

    fvdfvdfvgb

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("xa weqfgbnrhm,hjytjm,yujkh,mjhyutjhm,vnjut");
         System.out.println("jhj");
         System.out.println("ygthuhygbhgbhg");
-        System.out.println("EFZRgvefvg");
+        System.out.println("qsdcswef");
     }
 }
