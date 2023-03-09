5d15fbd0f9528e9728447cdc3e035640b8f40ea6
OneComputers <uxasanov798@gmail.com>
2023-03-09 19:07:10 +0500

    fvdfvdfvgb

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("xa weqfgbnrhm,hjytjm,yujkh,mjhyutjhm,vnjut");
+        System.out.println("jhj");
     }
 }
